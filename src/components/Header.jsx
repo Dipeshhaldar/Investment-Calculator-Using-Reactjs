099ebd421685ac840ba679d1b251ceb1633808a3
unknown <dipeshhaldar787@gmail.com>
2023-12-10 11:08:32 +0530

    Investment Calculator Project Completed

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import logo from'../assets/investment-calculator-logo.png';
+
+export default function() {
+    return (
+        <>
+        <header id='header'>
+            <img src={logo} alt="Logo Showing a Money Bag" />
+            <h1>Investment Calculator</h1>
+        </header>
+        </>
+    );
+}
